823898ec1580b477c3258ad8cc940c563a0a9a74
cosmin25 <tomsacosmin14@gmail.com>
2026-05-24 19:32:48 +0300

    feat: sidebar cu selector piata + brand dinamic

--- app.R
+++ app.R
@@ -16,7 +16,12 @@ library(scales)
 ui <- dashboardPage(
   skin = "blue",
   dashboardHeader(title = "Dashboard Auto SH"),
-  dashboardSidebar(),
+  dashboardSidebar(
+    selectInput("piata", "Selecteaza Piata:",
+                choices = c("SUA", "Germania", "India"),
+                selected = "Germania"),
+    uiOutput("brand_selector")
+  ),
   dashboardBody(
     h2("Dashboard in constructie")
   )
@@ -26,7 +31,25 @@ ui <- dashboardPage(
 # SERVER
 # --------------------------------------------------------------
 server <- function(input, output, session) {
-  # Va fi populat in pasii urmatori
+  # ---- Reactive: lista branduri pentru piata aleasa ----
+  brand_options <- reactive({
+    req(input$piata)
+    tabel <- switch(input$piata,
+                    "SUA" = "SUA_Cars_Cleaned",
+                    "Germania" = "Germany_Cars_Cleaned",
+                    "India" = "India_Cars_Cleaned")
+    con <- dbConnect(RSQLite::SQLite(), "identifier.sqlite")
+    rezultat <- dbGetQuery(con,
+      paste0("SELECT DISTINCT brand FROM ", tabel, " ORDER BY brand"))
+    dbDisconnect(con)
+    rezultat$brand
+  })
+
+  # ---- Dropdown brand dinamic ----
+  output$brand_selector <- renderUI({
+    selectInput("brand", "Selecteaza Brand:",
+                choices = brand_options())
+  })
 }
 
 # --------------------------------------------------------------
